f0ff31af776fdbccada35efe3a4837416ea65aa6
dkrym <krymdavid@gmail.com>
2020-09-09 19:29:26 +1000

    List of profiles

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const guestLinks = (
     <ul>
       <li>
-        <Link to='!#'>Developers</Link>
+        <Link to='/profiles'>Developers</Link>
       </li>
       <li>
         <Link to='/register'>Register</Link>
@@ -21,6 +21,9 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
 
   const authLinks = (
     <ul>
+      <li>
+        <Link to='/profiles'>Developers</Link>
+      </li>
       <li>
         <Link to='/dashboard'>
           <i className='fas fa-user'></i>{' '}
--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -2,7 +2,9 @@ import {
   GET_PROFILE,
   PROFILE_ERROR,
   CLEAR_PROFILE,
-  UPDATE_PROFILE
+  UPDATE_PROFILE,
+  GET_PROFILES,
+  GET_REPOS
 } from '../actions/types';
 
 const initialState = {
@@ -25,11 +27,17 @@ export default function (state = initialState, action) {
         loading: false
       };
 
+    case GET_PROFILES:
+      return { ...state, profiles: payload, loading: false };
+
     case PROFILE_ERROR:
       return { ...state, error: payload, loading: false };
 
     case CLEAR_PROFILE:
       return { ...state, profile: null, repos: [], loading: false };
+
+    case GET_REPOS:
+      return { ...state, repos: payload, loading: false };
 
     default:
       return state;
